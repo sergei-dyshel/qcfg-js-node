be63f08d400a7890e446990d63699d1fa1b42fd7
Sergei Dyshel <sergei@dyshel.com>
2025-03-23 09:50:53 +0200

    logging: allow using default console

--- src/lib/logging/appender.ts
+++ src/lib/logging/appender.ts
@@ -8,13 +8,24 @@ export class ConsoleAppender implements LogAppender {
   console: Console;
 
   constructor(options?: {
-    /** Allow writing to stdout, otherwise will use stderr for all logging */
+    /**
+     * Use default global console object, otherwise create a new console
+     *
+     * NOTE: need to use that for proper object dumps to DevTools console
+     */
+    default?: boolean;
+    /**
+     * Allow writing to stdout, otherwise will use stderr for all logging. Only used when `default`
+     * is false.
+     */
     allowStdout?: boolean;
   }) {
-    this.console = new Console({
-      stdout: options?.allowStdout ? process.stdout : process.stderr,
-      stderr: process.stderr,
-    });
+    if (options?.default) this.console = console;
+    else
+      this.console = new Console({
+        stdout: options?.allowStdout ? process.stdout : process.stderr,
+        stderr: process.stderr,
+      });
   }
   append(logLine: string, args?: unknown[]) {
     args = args ?? [];
