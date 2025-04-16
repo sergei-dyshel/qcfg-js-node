b627034fbca233027bbcd960fb0cf93955278b2e
Sergei Dyshel <sergei@dyshel.com>
2025-04-16 09:46:06 +0300

    axios: more exports

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,5 @@
 import type { AxiosError } from "axios";
-
-export { AxiosError } from "axios";
+export { AxiosError, type AxiosInstance } from "axios";
 
 export function isAxiosError(error: unknown): error is AxiosError {
   return error instanceof Error && error.name === "AxiosError";
