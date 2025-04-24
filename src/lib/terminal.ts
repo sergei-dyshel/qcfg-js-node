3e273af9135586ce696e6c374b1544f2f02969bc
Sergei Dyshel <sergei@dyshel.com>
2025-04-24 11:14:42 +0300

    add cli-progress

---
+++ src/lib/terminal.ts
@@ -0,0 +1,63 @@
+import {
+  MultiBar as CliMultiBar,
+  type SingleBar as CliSingleBar,
+  type Options,
+  Presets,
+} from "cli-progress";
+import { AsyncContext } from "./async-context";
+import { LineBufferedTransform } from "./stream";
+
+export { Presets as BarPresets };
+
+/**
+ * See {@link https://www.npmjs.com/package/cli-progress}.
+ */
+export class MultiBar<T extends object> extends CliMultiBar {
+  declare create: (
+    total: number,
+    startValue: number,
+    payload?: T,
+    barOptions?: Options,
+  ) => SingleBar<T>;
+}
+
+export interface SingleBar<T extends object> extends CliSingleBar {
+  update(current: number, payload?: T): void;
+  update(payload: T): void;
+  start(total: number, startValue: number, payload?: T): void;
+}
+
+export async function withProgressBar<M extends object, T>(
+  bar: MultiBar<M> | undefined,
+  callback: () => Promise<T>,
+) {
+  return bar
+    ? AsyncContext.run(progressBarLog(bar), async () => {
+        try {
+          return await callback();
+        } finally {
+          bar.stop();
+        }
+      })
+    : await callback();
+}
+
+function progressBarLog<M extends object>(bar: MultiBar<M>): AsyncContext.SimpleModifier {
+  const logTransform = new LineBufferedTransform(
+    (line) => {
+      bar.log(line + "\n");
+      return line;
+    },
+    {
+      forceEndingEOL: true,
+    },
+  );
+  return (context) => [
+    {
+      stdout:
+        context.stdout === process.stdout && process.stdout.isTTY ? logTransform : context.stdout,
+      stderr:
+        context.stderr === process.stderr && process.stderr.isTTY ? logTransform : context.stderr,
+    },
+  ];
+}
